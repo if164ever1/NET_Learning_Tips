f40011d1fbf14d1e6946fec2f80a0fb61831a0cf
if164ever1 <acere5ihor@gmail.com>
2023-01-22 16:25:57 +0100

    //LINQ WITH OBJECTS

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace Linq
 {
+    public class Bird
+    {
+        public string Name { get; set; }
+        public string  Color { get; set; }
+        public int Sightings { get; set; }
+    }
+
     internal static class Program
     {
         public static void Main(string[] args)
@@ -17,8 +25,24 @@ namespace Linq
             }
             
             
-            
-            
+            //LINQ WITH OBJECTS
+            List<Bird> birds = new List<Bird>
+            {
+                new Bird{Name = "Cardinal", Color = "Red", Sightings = 3},
+                new Bird{Name = "Dove",Color = "White", Sightings = 2},
+                new Bird{Name = "Robin", Color = "Red", Sightings = 5},
+                new Bird{Name = "Blue Jay", Color = "Blue", Sightings = 1},
+                new Bird{Name = "Canary", Color = "Yellow", Sightings = 0}
+            };
+            var redColor = birds.Where(bird => bird.Color == "Red").Select(x => x.Name);
+            foreach (var number in redColor)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+
         }
     }
 }
