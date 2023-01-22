24b2703266bd0832db89183196f34e45751f1882
if164ever1 <acere5ihor@gmail.com>
2023-01-22 18:00:46 +0100

    simple delegates

---
+++ .net/Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal abstract class Program
+    {
+        //private delegate void SayHello(string name);
+
+        private static void Hello(string name)
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
+
+        private static void Goodbye(string name)
+        {
+            Console.WriteLine($"Goodbye, {name}");
+        }
+        
+        public static void Main(string[] args)
+        {
+            //var sayHello = new SayHello(Hello);
+            Action<string> sayHello;
+            //Anonymous delegate
+            sayHello = delegate(string name1)
+            {
+                Console.WriteLine($"Hello, {name1}");
+            };
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            sayHello(name);
+            sayHello = delegate(string name1)
+            {
+                Console.WriteLine($"Goodbye, {name}");
+            };
+            sayHello(name);
+            
+            
+            
+        }
+    }
+}
