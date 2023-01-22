1eb0a2401a4bc0dc6990628e39241279c90749b4
if164ever1 <acere5ihor@gmail.com>
2023-01-22 16:04:58 +0100

    linq where

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //WHERE
+            List<int> numbers = new List<int>{2,4,6,8,16,32,64};
+            var numberGreaterThanTen = numbers.Where(x => x > 10);
+            foreach (var number in numberGreaterThanTen)
+            {
+                Console.WriteLine(number);
+            }
+            
+            
+            
+            
+        }
+    }
+}
